cc2ba3e7bea9b8373a260d6604aefbab405f790d
fedecame <fede.cameriere@gmail.com>
2020-05-03 18:58:52 -0300

    Comandos mas avanzados, faltan algunos..los principales ya estan.

--- visado_1/commandHandler.js
+++ visado_1/commandHandler.js
@@ -1,6 +1,4 @@
 const Printer = require('./printer');
-const {NoMatchingAlbumException, NoMatchingArtistException,
-  NoMatchingTrackException, NoMatchingPlaylistException} = require('./exceptions.js');
 
 class CommandHandler {
   constructor(args) {
@@ -54,6 +52,19 @@ class CommandHandler {
           console.error(exception);
         }
       },
+      //formato: addUser userName
+      addUser: function(unqfy) {
+        const userData = {
+          name: commandArgs[1]
+        };
+
+        try {
+          const user = unqfy.addUser(userData);
+          printer.printMessage(`Nuevo usuario (con id: ${user.id}) creado exitosamente`);
+        } catch (exception) {
+          console.error(exception);
+        }
+      },
 
       // DELETERS
       //formato: deleteArtist artistId
@@ -94,52 +105,36 @@ class CommandHandler {
       },
       //formato: getArtist artistId
       getArtist: function(unqfy) {
-        const artist = unqfy.getArtistById(commandArgs[1]);
         try {
-          if (artist === undefined) {
-            throw new NoMatchingArtistException(commandArgs[1]);
-          } else {
-            printer.printEntity(`Artista de id ${commandArgs[1]}`, artist);
-          }
+          const artist = unqfy.getArtistById(commandArgs[1]);
+          printer.printEntity(`Artista de id ${commandArgs[1]}`, artist);
         } catch (exception) {
           console.error(exception);
         }
       },
       //formato: getAlbum albumId
       getAlbum: function(unqfy) {
-        const album = unqfy.getAlbumById(commandArgs[1]);
         try {
-          if (album === undefined) {
-            throw new NoMatchingAlbumException(commandArgs[1]);
-          } else {
-            printer.printEntity(`Album de id ${commandArgs[1]}`, album);
-          }
+          const album = unqfy.getAlbumById(commandArgs[1]);
+          printer.printEntity(`Album de id ${commandArgs[1]}`, album);
         } catch (exception) {
           console.error(exception);
         }
       },
       //formato: getTrack trackId
       getTrack: function(unqfy) {
-        const track = unqfy.getTrackById(commandArgs[1]);
         try {
-          if (track === undefined) {
-            throw new NoMatchingTrackException(commandArgs[1]);
-          } else {
-            printer.printEntity(`Track de id ${commandArgs[1]}`, track);
-          }
+          const track = unqfy.getTrackById(commandArgs[1]);
+          printer.printEntity(`Track de id ${commandArgs[1]}`, track);
         } catch (exception) {
           console.error(exception);
         }
       },
       //formato: getPlaylist playlistId
       getPlaylist: function(unqfy) {
-        const playlist = unqfy.getPlaylistById(commandArgs[1]);
         try {
-          if (playlist === undefined) {
-            throw new NoMatchingPlaylistException(commandArgs[1]);
-          } else {
-            printer.printEntity(`Playlist de id ${commandArgs[1]}`, playlist);
-          }
+          const playlist = unqfy.getPlaylistById(commandArgs[1]);
+          printer.printEntity(`Playlist de id ${commandArgs[1]}`, playlist);
         } catch (exception) {
           console.error(exception);
         }
@@ -148,20 +143,16 @@ class CommandHandler {
       // SEARCHERS
       //formato: getTracksByArtist artistId
       getTracksByArtist: function(unqfy) {
-        const artist = unqfy.getArtistById(commandArgs[1]);
         try {
-          if (artist === undefined) {
-            throw new NoMatchingArtistException(commandArgs[1]);
-          } else {
-            printer.printArray(`Tracks del artista de id ${commandArgs[1]}`, unqfy.getTracksMatchingArtist(artist));
-          }
+          const artist = unqfy.getArtistById(commandArgs[1]);
+          printer.printArray(`Tracks del artista de id ${commandArgs[1]}`, unqfy.getTracksMatchingArtist(artist));
         } catch (error) {
           console.error(error);
         }
       },
       //formato: getTracksByGenres genre1 genre2 .. genreN
       getTracksByGenres: function(unqfy) {
-        printer.printArray(`Tracks de los siguientes generos: ${this._command.splice(1)}`, unqfy.getTracksMatchingGenres(this._command.splice(1)));
+        printer.printArray(`Tracks de los siguientes generos: ${commandArgs.splice(1)}`, unqfy.getTracksMatchingGenres(commandArgs.splice(1)));
       },
       //formato: searchAllPartialName stringToSearch
       searchAllPartialName: function(unqfy) {
@@ -171,12 +162,63 @@ class CommandHandler {
         printer.printArray('Tracks encontrados', allMatches.tracks);
         printer.printArray('Playlists encontrados', allMatches.playlists);
       },
+      //formato: searchTracksPartialName stringToSearch
+      searchTracksPartialName: function(unqfy) {
+        printer.printArray('Tracks encontrados', unqfy.getPartialMatchingTracks(commandArgs[1])); 
+      },
+      //formato: searchAlbumsPartialName stringToSearch
+      searchAlbumsPartialName: function(unqfy) {
+        printer.printArray('Albumes encontrados', unqfy.getPartialMatchingAlbums(commandArgs[1]));
+      },
+      //formato: searchArtistsPartialName stringToSearch
+      searchArtistsPartialName: function(unqfy) {
+        printer.printArray('Artistas encontrados', unqfy.getPartialMatchingArtists(commandArgs[1]));
+      },
+      //formato: searchPlaylistsPartialName stringToSearch
+      searchPlaylistsPartialName: function(unqfy) {
+        printer.printArray('Playlists encontrados', unqfy.getPartialMatchingPlaylists(commandArgs[1]));
+      },
+      //formato: top3TracksFromArtist artistId
+      top3TracksFromArtist: function(unqfy) {
+        try {
+          const tracks = unqfy.artistTopThreeTracks(commandArgs[1]);
+          printer.printArray('Top 3 Tracks', tracks);
+        } catch (exception) {
+          console.error(exception);
+        }
+      },
+      //formato: getListenedTracksByUser userId
+      getListenedTracksByUser: function(unqfy) {
+        /**
+         * TODO: Revisar el nombre del mensaje en unqfy!!
+         */
+        try {
+          const tracks = unqfy.getListenedTracksByUser(commandArgs[1]);
+          printer.printArray(`Tracks escuchados por el usuario de id ${commandArgs[1]}`, tracks);
+        } catch (exception) {
+          console.error(exception);
+        }
+      },
+      //formato: timesListenedTrackByUser
+      timesListenedTrackByUser: function(unqfy) {
+
+      },
 
       // CUSTOMS
       //formato: generatePlaylists playlistName maxDuration genre1 genre2 .. genreN
       generatePlaylists: function(unqfy) {
-        printer.printEntity('Playlist generado', unqfy.createPlaylist(commandArgs[1], this._command.slice(3), this._command[2]));
-      }
+        printer.printEntity('Playlist generado', unqfy.createPlaylist(commandArgs[1], commandArgs.slice(3), commandArgs[2]));
+      },
+
+
+
+      //formato: userListenTrack userId trackId
+      userListenTrack: function(unqfy) {
+        /**TODO 
+         * unqfy.userListenTrack(commandArgs[1], commandArgs[2]);
+         */
+      },
+
 
     };
   }
